758c28fac9bcbcd5c2e881740a9a9f6c1f3f091d
Nahdaa <jnahdaa@gmail.com>
2023-03-26 00:18:48 +0000

    Created a running script consisting of a story and inputs for the user to play the game.

---
+++ madlibsScript.py
@@ -0,0 +1,37 @@
+## MADLIBS PROJECT
+# Author: Nahdaa Jawed
+# Date Started: 25/03/2023 23:45
+# Date Finished: -
+# Description:
+
+#print("Which story line would you like to use?\nThe Witch\n") #Use if, elseif, else
+print("Please enter the following:\n")
+
+print("Your favourite colour: ")
+colour = input()
+print("\nAdjective: ")
+adj2 = input()
+print("\nAdjective: ")
+adj3 = input()
+print("\nAction Verb: ")
+verb1 = input()
+print("\nYour favourite animal: ")
+animal = input()
+print("\nAdjective: ")
+adj4 = input()
+print("\nAdjective: ")
+adj5 = input()
+print("\nAdjective: ")
+adj6 = input()
+
+
+print("\nThere was once a " + colour + " cottage deep in a " + adj2 + " forest. \nIt was home to a " + adj3 + " witch who loved to " + verb1 + ".\n"
+      "She was loved by all the animals in the forest, especially the " + animal + ".\n"
+      "However, the residents of the " + adj4 + " village nearby hated her because she was " + adj5 + ", \nbut she did not care, and sipped her " + adj6 + " tea "
+      "peacefully on her porch everyday. :)\nThe End!")
+
+#print("")
+
+#print("Would you like to play again? (y/n)")
+#playAgain = input()
+#if playAgain == "y"
